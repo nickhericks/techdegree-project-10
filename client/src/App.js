5ac74ba989615fca7759e16abf0206b2acca0ac9
Nick-Hericks <nickhericks@gmail.com>
2019-06-18 17:04:52 +0200

    Add fetch request to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor() {
+		super();
+		this.state = {
+			gifs: [],
+			loading: true,
+			courses: []
+		};
+	} 
+
+
+	componentDidMount() {
+		fetch("http://localhost:5000/api/courses")
+		.then(response => response.json())
+		.then(responseData => {
+			console.log(responseData);
+			this.setState({ courses: responseData });
+			console.log(this.state.courses);
+		})
+		.catch(error =>
+			console.log("Error fetching or parsing data", error)	
+		);
+	}
+
+	const currentCourses = this.state.courses.map( course => (
+		<li>{course.title}</li>
+	));
+
+	render() {
+		console.log(this.state.courses);
+
+
+
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<ul>
+						{currentCourses}
+					</ul>
+
+				</header>
+			</div>
+		);
+	}
 }
 
-export default App;
+// export default App;
