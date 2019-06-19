cf8f90575c67f4cbf1d1ed7fc9bf80880a640560
Nick-Hericks <nickhericks@gmail.com>
2019-06-19 18:55:43 +0200

    Build coursedetail structure

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -2,69 +2,73 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
 
-const CourseDetail = ({ match }) => {
-  let id = match.params.id;
-  return (
-    <div className="main-content">
-      <h2>{id} </h2>
-      {/* <p>
-        Introducing <strong>{name}</strong>, a teacher who loves teaching
-        courses about <strong>{topic}</strong>!
-      </p> */}
-    </div>
-  );
-};
+export default class CourseDetail extends Component {
+  constructor() {
+    super();
+    this.state = {
+      course: {}
+    };
+  }
 
-export default CourseDetail;
+  componentDidMount() {
+    fetch(`http://localhost:5000/api/courses/${this.props.match.params.id}`)
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ course: responseData.course });
+        console.log(this.state.course);
+      })
+      .catch(error => console.log("Error fetching or parsing data", error));
+  }
 
+  render() {
+    console.log(this.state.course);
+    console.log('Props', this.props);
+    console.log(this.props.match.params.id);
 
-// export default class CourseDetail extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       course: {}
-//     };
-//   }
+    const title = this.state.course.title;
+    const description = this.state.course.description;
 
-//   componentDidMount() {
-//     fetch("http://localhost:5000/api/courses")
-//       .then(response => response.json())
-//       .then(responseData => {
-//         this.setState({ courses: responseData.courses });
-//         console.log(this.state.courses);
-//       })
-//       .catch(error => console.log("Error fetching or parsing data", error));
-//   }
+		let materials;
 
-//   render() {
-//     console.log(this.state.course);
-//     console.log('Props', this.props);
+		let buttons; 		
+		// If current user is owner, add 'update' and 'delete' buttons
 
-//     let currentCourses;
-//     // If array is not empty, iterate over it
-//     // if (this.state.courses.length > 0) {
-//     //   currentCourses = this.state.courses.map(course => (
-//     //     <CourseCard id={course.id} key={course.id} title={course.title} />
-//     //   ));
-//     // } else {
-//     //   // If array is empty, display NoPics component
-//     //   // currentCourses = "";
-//     // }
+		
 
-//     return (
-//       <div className="photo-container">
-//         {/* Render images */}
-//         <ul className="course-list">
-//           {/* {currentCourses} */}
+    return (
+      <div>
+        <div className="details-subheader">
 
-//           <li className="course-card" id="new-course-card">
-//             <NavLink to={`/courses/create`}>
-//               <div className="new-course-card-title">+ Add New Course</div>
-//             </NavLink>
-//           </li>
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
+        <ul className="details-buttons">
+					{/* if current user is owner, add 'update' and 'delete' buttons */}
+					{buttons}
+
+          <li className="button" id="return-btn">
+            <NavLink to={`/`}>
+              <div className="">Return to List</div>
+            </NavLink>
+          </li>
+        </ul>
+
+				</div>
+        <div className="details-page">
+          <div className="details-main">
+            <h4>Course</h4>
+            <h1>{title}</h1>
+						{/* <h4>{this.state.course.user.firstName}</h4> */}
+						<p>{description}</p>
+          </div>
+          <div className="details-side">
+						<h5>Estimated Time</h5>
+						<p>14 hours</p>
+						<h5>Materials Needed</h5>
+						<ul>
+							{materials}
+						</ul>
+					</div>
+        </div>
+      </div>
+    );
+  }
+}
 
