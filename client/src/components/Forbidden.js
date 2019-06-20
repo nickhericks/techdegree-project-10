be508815cec9b821c1a97341ec73f7a10e11a6ab
Nick-Hericks <nickhericks@gmail.com>
2019-06-20 18:38:04 +0200

    Update Forbidden component

---
+++ client/src/components/Forbidden.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+// Component for letting the user know that
+// they can't access the requested page.
+const Forbidden = () => {
+  return (
+    <div className="container">
+      <h1>Forbidden</h1>
+      <h3>Sorry! You can't access this page.</h3>
+      <NavLink to={`/`}>
+        <div className="">Return to home page</div>
+      </NavLink>
+    </div>
+  );
+};
+
+export default NotFound;
