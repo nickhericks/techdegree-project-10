0fc4aa9640a62e2a1f3bd3bf9cf7b8f013ccad00
Nick-Hericks <nickhericks@gmail.com>
2019-06-23 20:39:38 +0200

    Import Consumer

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Consumer } from "./Context";
 
 export default class UpdateCourse extends Component {
   constructor() {
